80dcef510bfc8a6dd478645170025cdb62111a2c
aym1028 <a0010288@gmail.com>
2021-08-24 14:33:06 +0000

    C++입출력

--- bin/ex_one.cpp
+++ bin/ex_one.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
-
+using namespace std;
 int main()
 {
-	
+	char irum[30];
+	cout << "성함, 전화번호를 입력하세요\n";
+	cin.getline(irum, sizeof(irum)); //이렇게 쓰면 공백도 입력가능
+	//cin >> irum;  //cin은 뛰어쓰기 등등 입력의 받지 못한다 공백의 입력받지 못한다
 	//출력
-	std::cout << " 구메금액을 입력하세여\n";
+	cout << " 구메금액을 입력하세여\n";
 	int caltot, calpoint;
 	
-	std::cin >>caltot;
+	cin >>caltot;
 	calpoint =caltot * 0.01;
-	std::cout << "포인트는 : " << calpoint << "입니다\n";
+	cout << irum << "님포인트는 : " << calpoint << "입니다\n";
 }
