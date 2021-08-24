2308636d9a6dea03fa29e1462a07006927326661
aym1028 <a0010288@gmail.com>
2021-08-24 14:20:00 +0000

    C++언어 입출력

---
+++ bin/ex_one.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+	
+	//출력
+	std::cout << " 구메금액을 입력하세여\n";
+	int caltot, calpoint;
+	
+	std::cin >>caltot;
+	calpoint =caltot * 0.01;
+	std::cout << "포인트는 : " << calpoint << "입니다\n";
+}
