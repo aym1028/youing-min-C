f6af764129b1ae7f50a406baf7336e1037e7ab0c
aym1028 <a0010288@gmail.com>
2021-09-13 14:11:25 +0000

    namespace2번째

--- bin/InlineFunc.cpp
+++ bin/InlineFunc.cpp
@@ -16,7 +16,7 @@ inline T SQUARE(T x)
 {
 	return x*x;
 }
-int main (void)gi
+int main (void)
 {
 	std::cout<<SQUARE(5.5)<<std::endl;
 	std::cout<<SQUARE(12)<<std::endl;
---
+++ bin/NameSp2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace BestComImp1
+{
+	void SimpleFunc(void);
+}
+
+namespace ProgcomImp1
+{
+	void SimpleFunc(void);
+}
+int main(void)
+{
+	BestComImp1::SimpleFunc();
+	ProgcomImp1::SimpleFunc();
+	return 0;
+	
+}
+
+void BestComImp1::SimpleFunc(void)
+{
+	std::cout<<"BestCom이 정의한 함수"<<std::endl;
+}
+void ProgcomImp1::SimpleFunc(void)
+{
+	std::cout<<"ProgcomImp이 정의한 함수"<<std::endl;
+}
